0964263eb7cccde511ee859bf62b6f24f177a1bb
Ashley Ma <ashleyma@gertrudpihelgas.ait-sydney.net.au>
2018-03-01 13:59:49 +1100

    2pm dashboard commit

--- dashboard.rb
+++ dashboard.rb
@@ -2,8 +2,20 @@
 require 'colorize'
 require 'terminal-table'
 
-require_relative 'expenses'
-require_relative 'revenue'
+##---arrays below
+revenue_array = [{Item: "Coffee", Month: "February", Amount: 50}, 
+    {Item: "Muffin", Month: "March", Amount: 30},
+    {Item: "Cake", Month: "February", Amount: 30},
+    {Item: "Salad", Month: "March", Amount: 30},
+    {Item: "water", Month: "February", Amount: 30},
+    ]
+
+expenses_array = [{Item: "Coffee", Month: "February", Amount: 30}, 
+    {Item: "Muffin", Month: "March", Amount: 30},
+    {Item: "Cake", Month: "February", Amount: 30},
+    {Item: "Salad", Month: "March", Amount: 30},
+    {Item: "water", Month: "February", Amount: 30},
+    ]
 
 ###----------Methods Below
 
@@ -19,8 +31,7 @@ def add array #Method for adding new entries into array
 end
 
 #----------------- CODE RUNS BELOW ------------------########
-total_revenue = Revenue::give.reduce(0) {|sum, revenue_item| sum += revenue_item[:Amount]} 
-total_expenses = Expenses::give.reduce(0) {|sum, revenue_item| sum += revenue_item[:Amount]} 
+
 while true #Ask questions for which option they would like; add, show, exit
     puts "What do you want to do"
     puts "Add a new revenue item (AR)"
@@ -32,13 +43,14 @@ while true #Ask questions for which option they would like; add, show, exit
     puts "Display yearly profit (SYP)"
     puts "Exit back to main menu (Exit)"
 choice = gets.chomp 
-
+total_revenue = revenue_array.reduce(0) {|sum, revenue_item| sum += revenue_item[:Amount]} 
+total_expenses = expenses_array.reduce(0) {|sum, revenue_item| sum += revenue_item[:Amount]} 
     case choice
     when "AR" #When they want to add new revenue item, collect information. 
-        add Revenue::give
+        add revenue_array
     when "SAR" #show all available months 
         rev_hash = Hash.new(0)
-        Revenue::give.each do |item|
+        revenue_array.each do |item|
             rev_hash[item[:Month].to_sym] += item[:Amount]
         end
         puts rev_hash
@@ -47,10 +59,10 @@ choice = gets.chomp
     when "SYR"
         puts total_revenue 
     when "AE"
-        add Expenses::give
+        add expenses_array
     when "SAE" 
         exp_hash = Hash.new(0)
-        Expenses::give.each do |item|
+        expenses_array.each do |item|
             exp_hash[item[:Month].to_sym] += item[:Amount]
         end
         puts exp_hash
